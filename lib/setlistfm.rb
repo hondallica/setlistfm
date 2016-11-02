893a5e644e4229a5137479dba6513cd25e25717b
Hondallica <hondallica@gmail.com>
2016-11-02 09:46:08 +0900

    fix url

--- lib/setlistfm.rb
+++ lib/setlistfm.rb
@@ -3,12 +3,13 @@ require "faraday_middleware"
 
 class Setlistfm
   VERSION = "0.0.1"
+  API_VERSION = "0.1"
   URL = "https://api.setlist.fm"
 
   attr_accessor :api_version, :url
 
   def initialize(url = URL)
-    @api_version = "0.1"
+    @api_version = API_VERSION
     @url = url
   end
 end
